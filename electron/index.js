2e024432f0c5929407aa3403603421e90e3f4743
Kinga <113030259+Kinga-9@users.noreply.github.com>
2022-10-07 08:32:05 +0200

    Index javascript and html
    This is not the final look, but for now it will work for integrating into the serve.py

--- electron/index.js
+++ electron/index.js
@@ -1,85 +1,38 @@
-document.onkeydown = updateKey;
-document.onkeyup = resetKey;
+var server_port=65432;
+var server_addr="192.168.68.106";
 
-var server_port = 65432;
-var server_addr = "192.168.1.133";   // the IP address of your Raspberry PI
 
-function client(){
-    
-    const net = require('net');
-    var input = document.getElementById("message").value;
 
-    const client = net.createConnection({ port: server_port, host: server_addr }, () => {
-        // 'connect' listener.
-        console.log('connected to server!');
-        // send the message
-        client.write(`${input}\r\n`);
+function move(direction){
+    const net=require('net');
+    //var input=document.getElementById("myName").value;
+    const client=net.createConnection({port: server_port, host: server_addr},() =>{
+    //connect listener
+    console.log('connected to server!');
+    client.write(direction);
+     //sock.send(bytes("forward", encoding='utf-8'))
     });
-    
-    // get the data from the server
     client.on('data', (data) => {
-        document.getElementById("bluetooth").innerHTML = data;
+        //message=JSON.parse(data)
+        receive_data(data)
         console.log(data.toString());
         client.end();
         client.destroy();
     });
-
-    if (!('bluetooth' in navigator)) {
-        console.log('Bluetooth API not supported on your browser.');
-    } else {
-        console.log('CONGRATS! Bluetooth API supported on your browser.');
-    }
-
+    
     client.on('end', () => {
         console.log('disconnected from server');
     });
-
-
 }
-
-// for detecting which key is been pressed w,a,s,d
-function updateKey(e) {
-
-    e = e || window.event;
-
-    if (e.keyCode == '87') {
-        // up (w)
-        document.getElementById("upArrow").style.color = "green";
-        // send_data("87");
-    }
-    else if (e.keyCode == '83') {
-        // down (s)
-        document.getElementById("downArrow").style.color = "green";
-        // send_data("83");
-    }
-    else if (e.keyCode == '65') {
-        // left (a)
-        document.getElementById("leftArrow").style.color = "green";
-        // send_data("65");
-    }
-    else if (e.keyCode == '68') {
-        // right (d)
-        document.getElementById("rightArrow").style.color = "green";
-        // send_data("68");
-    }
+function receive_data(data){
+    var json_d=JSON.parse(data)
+    document.getElementById("CPU_temp").innerHTML = json_d.cpu_temperature+" degrees";
+    document.getElementById("PI_temperature").innerHTML = json_d.gpu_temperature +" degrees";
+    document.getElementById("battery").innerHTML = json_d.battery+" %";
+    document.getElementById("ram").innerHTML = json_d.ram;
+    document.getElementById("usage").innerHTML = json_d.cpu_usage+ " %";
+    document.getElementById("direction").innerHTML = json_d.direction;
+    document.getElementById("speed").innerHTML = json_d.speed+ " m/h";
+    
 }
-
-// reset the key to the start state 
-function resetKey(e) {
-
-    e = e || window.event;
-
-    document.getElementById("upArrow").style.color = "grey";
-    document.getElementById("downArrow").style.color = "grey";
-    document.getElementById("leftArrow").style.color = "grey";
-    document.getElementById("rightArrow").style.color = "grey";
-}
-
-
-// update data for every 50ms
-function update_data(){
-    setInterval(function(){
-        // get image from python server
-        client();
-    }, 50);
-}
+     
